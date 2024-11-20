77ac6ea2d106e952765a42ac71c9b374815b4b3a
Marijn Huizendveld <marijn@huizendveld.com>
2024-11-20 23:15:25 +0100

    :alembic: Introduce variable.

--- src/Pricing/DurationOfTrip.spec.ts
+++ src/Pricing/DurationOfTrip.spec.ts
@@ -17,7 +17,10 @@ const durationOfTripFromString = (input: string): DurationOfTrip => {
 };
 
 const durationOfTripToString = (input: DurationOfTrip): string => {
-    return `${input.days.toString(10).padStart(2, "0")}d ${input.hours.toString(10).padStart(2, "0")}h ${input.minutes.toString(10).padStart(2, "0")}m`;
+    const daysWithLeadingZeroes = input.days.toString(10).padStart(2, "0");
+    const hoursWithLeadingZeroes = input.hours.toString(10).padStart(2, "0");
+
+    return `${daysWithLeadingZeroes}d ${hoursWithLeadingZeroes}h ${input.minutes.toString(10).padStart(2, "0")}m`;
 }
 
 describe('Duration of trip', () => {
