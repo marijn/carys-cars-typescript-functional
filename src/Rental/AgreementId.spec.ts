40404514431c5e578c65ab4479807f6c6ae4bfb4
Marijn Huizendveld <marijn@huizendveld.com>
2024-11-21 15:56:45 +0100

    :mag: `AgreementId`
     - `is composed of agreement prefix and unique identifier`

--- src/Rental/AgreementId.spec.ts
+++ src/Rental/AgreementId.spec.ts
@@ -13,6 +13,7 @@ const agreementIdToString: (agreementId: AgreementId) => string = (agreementId) 
 describe('Agreement ID', () => {
     const examples: string[] = [
         "agreement:11111111-1111-1111-1111-111111111111",
+        "agreement:ee5accf0-605c-4de7-95bb-623af10f5cd9",
     ];
 
     it.each(examples)('is composed of agreement prefix and unique identifier', (input: string) => {
