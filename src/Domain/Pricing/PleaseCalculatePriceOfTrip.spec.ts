29d1ae152f8b1ed8e0033881d8db68f067dc909a
Marijn Huizendveld <marijn@huizendveld.com>
2024-11-21 23:10:44 +0100

    :white_check_mark: `Please calculate price of trip`.
     - `Happy path :-)`

--- src/Domain/Pricing/PleaseCalculatePriceOfTrip.spec.ts
+++ src/Domain/Pricing/PleaseCalculatePriceOfTrip.spec.ts
@@ -4,9 +4,42 @@ import {PriceOfTripWasCalculated} from "./PriceOfTripWasCalculated";
 import {PleaseCalculatePriceOfTrip} from "./PleaseCalculatePriceOfTrip";
 import {durationOfTripFromString} from "./DurationOfTrip";
 import Dinero from "dinero.js";
+import {runAssertionOnDecider} from "../../Infrastructure/Decider/Testing/runAssertionOnDecider";
+import {Decider} from "../../Infrastructure/Decider/Decider";
 
 type PricingEvents = | PriceOfTripWasCalculated;
 type PricingCommands = | PleaseCalculatePriceOfTrip;
+type PricingStates = {}
+
+const decider: Decider<PricingCommands, PricingStates, PricingEvents, TripId> = {
+    decide(command: PricingCommands, state: PricingStates): Promise<PricingEvents[]> {
+        switch (command._named) {
+            case "Please calculate price of trip": {
+                return Promise.resolve([
+                    {
+                        _named: "Price of trip was calculated",
+                        tripId: "trip:11111111-1111-1111-1111-111111111111",
+                        agreementId: "agreement:11111111-1111-1111-1111-111111111111",
+                        durationOfTrip: durationOfTripFromString("00d 00h 17m"),
+                        tripDistance: "19.0 km",
+                        pricePerMinute: Dinero({amount: 35, currency: "EUR", precision: 2}),
+                        totalPrice: Dinero({amount: 490, currency: "EUR", precision: 2}),
+                        customerId: "customer:AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA",
+                    }
+                ]);
+            }
+        }
+    },
+    evolve(state: PricingStates, event: PricingEvents): PricingStates {
+        return state;
+    },
+    identify(command: PricingCommands): TripId {
+        throw new Error('TODO: Implement me');
+    },
+    initialState(): PricingStates {
+        return {};
+    }
+};
 
 describe('Please calculate price of trip', () => {
     const scenario = new CommandHandlingScenario<PricingEvents, PricingCommands>()
@@ -30,6 +63,7 @@ describe('Please calculate price of trip', () => {
                 pricePerMinute: Dinero({amount: 35, currency: "EUR", precision: 2}),
                 totalPrice: Dinero({amount: 490, currency: "EUR", precision: 2}),
                 customerId: "customer:AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA",
-            });
+            })
+            .assertScenario(runAssertionOnDecider(decider));
     });
 });
