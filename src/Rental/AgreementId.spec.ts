76739bda45990189e7c644f815a96dc7902b1062
Marijn Huizendveld <marijn@huizendveld.com>
2024-11-21 20:51:04 +0100

    :package: Move into dedicated file.

--- src/Rental/AgreementId.spec.ts
+++ src/Rental/AgreementId.spec.ts
@@ -1,41 +1,5 @@
 import {describe, expect, it} from "@jest/globals";
-
-type AgreementId = `agreement:${string}`
-
-class SorryUnsupportedAgreementId extends Error {
-    constructor(
-        msg: string,
-        public readonly input: string,
-    ) {
-        super(msg);
-
-        Object.setPrototypeOf(this, SorryUnsupportedAgreementId.prototype);
-    }
-
-    static becauseItDoesNotMatchTheExpectedFormat(input: string): SorryUnsupportedAgreementId {
-        return new SorryUnsupportedAgreementId(
-            'Sorry, the id does not match the expected format ("agreement:${uuid}")',
-            input
-        );
-    }
-}
-
-const agreementIdFromString: (input: string) => AgreementId = (input) => {
-    const parsed = input.match(/^agreement:(?<unique>\S*)$/);
-    // intentional usage of less constraint pattern to allow uuids human-readable UUIDs for testing
-    // (e.g. AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA or 11111111-1111-1111-1111-111111111111)
-    const poorPersonsUuidTest = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
-
-    if ( ! poorPersonsUuidTest.test(parsed.groups['unique'])) {
-        throw SorryUnsupportedAgreementId.becauseItDoesNotMatchTheExpectedFormat(input);
-    }
-
-    return `agreement:${parsed.groups['unique']}`;
-};
-
-const agreementIdToString: (agreementId: AgreementId) => string = (agreementId) => {
-    return agreementId;
-};
+import {agreementIdFromString, agreementIdToString, SorryUnsupportedAgreementId} from "./AgreementId";
 
 describe('Agreement ID', () => {
     const examples: string[] = [
---
+++ src/Rental/AgreementId.ts
@@ -0,0 +1,35 @@
+type AgreementId = `agreement:${string}`
+
+export class SorryUnsupportedAgreementId extends Error {
+    constructor(
+        msg: string,
+        public readonly input: string,
+    ) {
+        super(msg);
+
+        Object.setPrototypeOf(this, SorryUnsupportedAgreementId.prototype);
+    }
+
+    static becauseItDoesNotMatchTheExpectedFormat(input: string): SorryUnsupportedAgreementId {
+        return new SorryUnsupportedAgreementId(
+            'Sorry, the id does not match the expected format ("agreement:${uuid}")',
+            input
+        );
+    }
+}
+
+export const agreementIdFromString: (input: string) => AgreementId = (input) => {
+    const parsed = input.match(/^agreement:(?<unique>\S*)$/);
+    // intentional usage of less constraint pattern to allow uuids human-readable UUIDs for testing
+    // (e.g. AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA or 11111111-1111-1111-1111-111111111111)
+    const poorPersonsUuidTest = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+
+    if (!poorPersonsUuidTest.test(parsed.groups['unique'])) {
+        throw SorryUnsupportedAgreementId.becauseItDoesNotMatchTheExpectedFormat(input);
+    }
+
+    return `agreement:${parsed.groups['unique']}`;
+};
+export const agreementIdToString: (agreementId: AgreementId) => string = (agreementId) => {
+    return agreementId;
+};
