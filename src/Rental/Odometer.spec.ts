ff2929176ad244e2bc93112980cffd2aed0aedbb
Marijn Huizendveld <marijn@huizendveld.com>
2024-11-21 13:47:20 +0100

    :package: Rename type.

--- src/Rental/Odometer.spec.ts
+++ src/Rental/Odometer.spec.ts
@@ -1,15 +1,15 @@
 import {describe, expect, it} from "@jest/globals";
 
-type Odometer<
+type DistanceTraveled<
     BeforeDecimal extends number = number,
     AfterDecimal extends number = number
 > = `${BeforeDecimal}.${AfterDecimal} km`;
 
-const odometerToString: (input: Odometer) => string = (input) => {
+const odometerToString: (input: DistanceTraveled) => string = (input) => {
     return input;
 };
 
-const odometerFromString: (input: string) => Odometer = (input) => {
+const odometerFromString: (input: string) => DistanceTraveled = (input) => {
     const parsed = input.match(/^(?<beforeDecimal>[0-9]+).(?<afterDecimal>[0-9]) km$/);
     const beforeDecimal = parseInt(parsed.groups['beforeDecimal'], 10);
     const afterDecimal = parseInt(parsed.groups['afterDecimal'], 10);
@@ -17,7 +17,7 @@ const odometerFromString: (input: string) => Odometer = (input) => {
     return `${beforeDecimal}.${afterDecimal} km`;
 };
 
-const distanceTraveled: (start: Odometer, end: Odometer) => Odometer = (start, end) => {
+const distanceTraveled: (start: DistanceTraveled, end: DistanceTraveled) => DistanceTraveled = (start, end) => {
     return '37.6 km';
 };
 
@@ -35,12 +35,12 @@ describe('Odometer', () => {
     });
 
     it("calculates distance traveled", () => {
-        const start: Odometer = '1482.2 km';
-        const end: Odometer = '1519.8 km';
+        const start: DistanceTraveled = '1482.2 km';
+        const end: DistanceTraveled = '1519.8 km';
 
-        const actual: Odometer = distanceTraveled(start, end);
+        const actual: DistanceTraveled = distanceTraveled(start, end);
 
-        const expected: Odometer = '37.6 km';
+        const expected: DistanceTraveled = '37.6 km';
         expect(actual).toEqual(expected);
     });
 });
