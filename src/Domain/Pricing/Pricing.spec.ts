d30564353e8c1a696b3bde76a54dd3537850044e
Marijn Huizendveld <marijn@huizendveld.com>
2024-11-22 11:21:30 +0100

    :mag: `Pricing`
     - `Launching Pricing: Short trip`

--- src/Domain/Pricing/Pricing.spec.ts
+++ src/Domain/Pricing/Pricing.spec.ts
@@ -11,6 +11,8 @@ describe('Pricing', () => {
     describe('Launching Pricing', () => {
         const examples: [DistanceTraveled, DurationOfTrip, Dinero.Dinero][] = [
             ['19.0 km', durationOfTripFromString('00d 00h 17m'), Dinero({amount: 595, currency: "EUR", precision: 2})],
+            ['0.0 km', durationOfTripFromString('00d 00h 17m'), Dinero({amount: 595, currency: "EUR", precision: 2})],
+            ['121.6 km', durationOfTripFromString('00d 03h 03m'), Dinero({amount: 6405, currency: "EUR", precision: 2})],
         ];
 
         it.each(examples)("Short trip (trip distance = %s, trip duration = %s, total price = %s)", (
