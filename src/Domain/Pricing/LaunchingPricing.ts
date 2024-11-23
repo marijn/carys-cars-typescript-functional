2893bc28e72ad18092685f0e96e791cf62c48eaf
Marijn Huizendveld <marijn@huizendveld.com>
2024-11-23 08:13:17 +0100

    :alembic: Extract function.

--- src/Domain/Pricing/LaunchingPricing.ts
+++ src/Domain/Pricing/LaunchingPricing.ts
@@ -11,11 +11,19 @@ export const launchingPricing: PricingPolicy = (
     const pricePerMinute = Dinero({amount: 25, currency: "EUR", precision: 2});
     const pricePerAdditionalKilometer = Dinero({amount: 11, currency: "EUR", precision: 2});
     const includedDistance: DistanceTraveled = "250.0 km";
-    const exceededOrRemaining = calculateDistanceTraveled(tripDistance, includedDistance);
-    const multiplier = Math.floor(Math.abs(parseFloat(exceededOrRemaining)));
-    const additionalDistanceCharge = exceededOrRemaining.startsWith('-')
-        ? pricePerAdditionalKilometer.multiply(multiplier)
-        : Dinero({amount: 0, currency: "EUR", precision: 2});
 
-    return pricePerMinute.multiply(durationOfTripToTotalMinutes(tripDuration)).add(additionalDistanceCharge);
+    const flatFeePricing: PricingPolicy = (
+        tripDistance,
+        tripDuration
+    ) => {
+        const exceededOrRemaining = calculateDistanceTraveled(tripDistance, includedDistance);
+        const multiplier = Math.floor(Math.abs(parseFloat(exceededOrRemaining)));
+        const additionalDistanceCharge = exceededOrRemaining.startsWith('-')
+            ? pricePerAdditionalKilometer.multiply(multiplier)
+            : Dinero({amount: 0, currency: "EUR", precision: 2});
+
+        return pricePerMinute.multiply(durationOfTripToTotalMinutes(tripDuration)).add(additionalDistanceCharge);
+    };
+
+    return flatFeePricing(tripDistance, tripDuration);
 };
