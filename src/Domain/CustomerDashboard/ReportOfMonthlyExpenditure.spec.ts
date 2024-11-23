772602a0889f033a0a199fb05f18912d66155d2a
Marijn Huizendveld <marijn@huizendveld.com>
2024-11-23 22:09:47 +0100

    :package: Move into dedicated package.

--- src/Domain/CustomerDashboard/ReportOfMonthlyExpenditure.spec.ts
+++ src/Domain/CustomerDashboard/ReportOfMonthlyExpenditure.spec.ts
@@ -1,91 +1,22 @@
 import {describe, it} from "@jest/globals";
 import {QueryHandlingScenario} from "../../Infrastructure/Projector/Testing/QueryHandlingScenario";
 import Dinero from "dinero.js";
-import {AgreementId} from "../Rental/AgreementId";
 import {durationOfTripFromString} from "../Pricing/DurationOfTrip";
 import {LocalDateTime, ZonedDateTime, ZoneId} from "js-joda";
 import {runAssertionsOnProjector} from "../../Infrastructure/Projector/Testing/runAssertionsOnProjector";
 import {
-    AllMonths,
-    AllYears,
-    EndedRentalAgreement,
-    ReportingMonth,
     ReportOfMonthlyExpenditureAnswers,
     ReportOfMonthlyExpenditureEvents,
     ReportOfMonthlyExpenditureProjector,
-    ReportOfMonthlyExpenditureQueries,
-    Trip
+    ReportOfMonthlyExpenditureQueries
 } from "./ReportOfMonthlyExpenditure";
+import {inMemoryReportOfMonthlyExpenditureProjector} from "./InMemoryReportOfMonthlyExpenditureProjector";
 
 const scenario = new QueryHandlingScenario<
     ReportOfMonthlyExpenditureEvents,
     ReportOfMonthlyExpenditureQueries,
     ReportOfMonthlyExpenditureAnswers
 >();
-
-const inMemoryReportOfMonthlyExpenditureProjector: () => ReportOfMonthlyExpenditureProjector = () => {
-    const agreementsByAgreementId: { [key: AgreementId]: EndedRentalAgreement } = {};
-    const tripsByCustomer: { [key: CustomerId]: Partial<Record<ReportingMonth, Trip[]>> } = {};
-
-    return {
-        async ask(query: ReportOfMonthlyExpenditureQueries): Promise<ReportOfMonthlyExpenditureAnswers> {
-            return {
-                _named: "Report of monthly expenditure by customer",
-                customerId: query.customerId,
-                month: query.month,
-                trips: query.customerId in tripsByCustomer
-                    ? (query.month in tripsByCustomer[query.customerId] ? tripsByCustomer[query.customerId][query.month]: [])
-                    : []
-            };
-        },
-        async when(event: ReportOfMonthlyExpenditureEvents): Promise<void> {
-            switch (event._named) {
-                case "Rental ended": {
-                    agreementsByAgreementId[event.agreementId] = {
-                        odometerEnd: event.odometerEnd,
-                        odometerStart: event.odometerStart,
-                        rentalEnded: event.rentalEnded,
-                        rentalStarted: event.rentalStarted,
-                        startPosition: event.startPosition,
-                        endPosition: event.endPosition,
-                        month: `${event.rentalStarted.year() as unknown as AllYears}-${event.rentalStarted.month().value().toString(10).padStart(2, '0') as unknown as AllMonths}`
-                    }
-
-                    return;
-                }
-                case "Price of trip was calculated": {
-                    const agreement = agreementsByAgreementId[event.agreementId];
-
-                    if ( ! (event.customerId in tripsByCustomer)) {
-                        tripsByCustomer[event.customerId] = {};
-                    }
-
-                    if ( ! (agreement.month in tripsByCustomer[event.customerId])) {
-                        tripsByCustomer[event.customerId][agreement.month] = [];
-                    }
-
-                    tripsByCustomer[event.customerId][agreement.month].push({
-                        agreementId: event.agreementId,
-                        durationOfTrip: event.durationOfTrip,
-                        odometerEnd: agreement.odometerEnd,
-                        odometerStart: agreement.odometerStart,
-                        rentalEnded: agreement.rentalEnded,
-                        rentalStarted: agreement.rentalStarted,
-                        startPosition: agreement.startPosition,
-                        endPosition: agreement.endPosition,
-                        totalPrice: event.totalPrice,
-                        tripDistance: event.tripDistance,
-                        tripId: event.tripId,
-                        vehicle: event.vehicle
-                    });
-
-                    return;
-                }
-            }
-        },
-        subscribesTo: [],
-    };
-}
 
 const testReportOfMonthlyExpenditureProjector: (subjectUnderTest: () => ReportOfMonthlyExpenditureProjector) => void = (subjectUnderTest) => {
     describe('Get report of monthly expenditure by customer', () => {
