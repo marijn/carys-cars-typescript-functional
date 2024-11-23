86b8e347499c392265b9bd6189f4519e10bb815a
Marijn Huizendveld <marijn@huizendveld.com>
2024-11-23 22:15:18 +0100

    :eyeglasses: Follow coding standard.

--- src/Domain/CustomerDashboard/InMemoryReportOfMonthlyExpenditureProjector.ts
+++ src/Domain/CustomerDashboard/InMemoryReportOfMonthlyExpenditureProjector.ts
@@ -30,7 +30,12 @@ export const inMemoryReportOfMonthlyExpenditureProjector: () => ReportOfMonthlyE
             switch (event._named) {
                 case "Rental ended": {
                     const year = event.rentalStarted.year() as unknown as AllYears;
-                    const monthWithLeadingZero = event.rentalStarted.month().value().toString(10).padStart(2, '0') as unknown as AllMonths;
+                    const monthWithLeadingZero = event
+                        .rentalStarted
+                        .month()
+                        .value()
+                        .toString(10)
+                        .padStart(2, '0') as unknown as AllMonths;
 
                     agreementsByAgreementId[event.agreementId] = {
                         odometerEnd: event.odometerEnd,
