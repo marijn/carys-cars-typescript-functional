bc8ec1f86b704377c70b5bd3c6c7a52af3a9816d
Marijn Huizendveld <marijn@huizendveld.com>
2024-11-23 08:19:26 +0100

    :alembic: Extract factory function.

--- src/Domain/Pricing/LaunchingPricing.ts
+++ src/Domain/Pricing/LaunchingPricing.ts
@@ -4,6 +4,22 @@ import Dinero from "dinero.js";
 
 export type PricingPolicy = (tripDistance: DistanceTraveled, tripDuration: DurationOfTrip) => Dinero.Dinero;
 
+export const flatFeePricingWith: (
+    pricePerMinute: Dinero.Dinero,
+    pricePerAdditionalKilometer: Dinero.Dinero,
+    includedDistance: DistanceTraveled
+) => PricingPolicy = (pricePerMinute, pricePerAdditionalKilometer, includedDistance) => {
+    return (tripDistance, tripDuration) => {
+        const exceededOrRemaining = calculateDistanceTraveled(tripDistance, includedDistance);
+        const multiplier = Math.floor(Math.abs(parseFloat(exceededOrRemaining)));
+        const additionalDistanceCharge = exceededOrRemaining.startsWith('-')
+            ? pricePerAdditionalKilometer.multiply(multiplier)
+            : Dinero({amount: 0, currency: "EUR", precision: 2});
+
+        return pricePerMinute.multiply(durationOfTripToTotalMinutes(tripDuration)).add(additionalDistanceCharge);
+    }
+};
+
 export const launchingPricing: PricingPolicy = (
     tripDistance,
     tripDuration
@@ -12,18 +28,7 @@ export const launchingPricing: PricingPolicy = (
     const pricePerAdditionalKilometer = Dinero({amount: 11, currency: "EUR", precision: 2});
     const includedDistance: DistanceTraveled = "250.0 km";
 
-    const flatFeePricing: PricingPolicy = (
-        tripDistance,
-        tripDuration
-    ) => {
-        const exceededOrRemaining = calculateDistanceTraveled(tripDistance, includedDistance);
-        const multiplier = Math.floor(Math.abs(parseFloat(exceededOrRemaining)));
-        const additionalDistanceCharge = exceededOrRemaining.startsWith('-')
-            ? pricePerAdditionalKilometer.multiply(multiplier)
-            : Dinero({amount: 0, currency: "EUR", precision: 2});
-
-        return pricePerMinute.multiply(durationOfTripToTotalMinutes(tripDuration)).add(additionalDistanceCharge);
-    };
+    const flatFeePricing: PricingPolicy = flatFeePricingWith(pricePerMinute, pricePerAdditionalKilometer, includedDistance);
 
     return flatFeePricing(tripDistance, tripDuration);
 };
