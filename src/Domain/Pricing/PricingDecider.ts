f0837a7f7a6c56df39b1a8821b52d941d8469b0e
Marijn Huizendveld <marijn@huizendveld.com>
2024-11-24 10:09:56 +0100

    :package: Move into dedicated package.

---
+++ src/Domain/Pricing/PricingDecider.ts
@@ -0,0 +1,71 @@
+import {PriceOfTripWasCalculated} from "./PriceOfTripWasCalculated";
+import {PleaseCalculatePriceOfTrip} from "./PleaseCalculatePriceOfTrip";
+import {launchPricingPerMinute, PricingPolicy} from "./LaunchingPricing";
+import {Decider} from "../../Infrastructure/Decider/Decider";
+import {TripId} from "./TripId";
+
+type TripIsNotPriced = Readonly<{
+    _named: "TripIsNotPriced",
+}>;
+
+type TripIsPriced = Readonly<{
+    _named: "TripIsPriced",
+}>;
+
+export type PricingEvents = | PriceOfTripWasCalculated;
+
+export type PricingCommands = | PleaseCalculatePriceOfTrip;
+
+type PricingStates =
+    | TripIsNotPriced
+    | TripIsPriced;
+
+export const buildPricingDecider: (pricingPolicy: PricingPolicy) => Decider<PricingCommands, PricingStates, PricingEvents, TripId> = (
+    pricingPolicy
+) => {
+    return {
+        decide(command: PricingCommands, state: PricingStates): Promise<PricingEvents[]> {
+            switch (command._named) {
+                case "Please calculate price of trip": {
+                    if ('TripIsPriced' === state._named) {
+                        return Promise.resolve([]);
+                    }
+
+                    return Promise.resolve([
+                        {
+                            _named: "Price of trip was calculated",
+                            tripId: command.tripId,
+                            vehicle: command.vehicle,
+                            agreementId: command.agreementId,
+                            durationOfTrip: command.durationOfTrip,
+                            tripDistance: command.tripDistance,
+                            pricePerMinute: launchPricingPerMinute,
+                            totalPrice: pricingPolicy(command.tripDistance, command.durationOfTrip),
+                            customerId: command.customerId,
+                        }
+                    ]);
+                }
+            }
+        },
+        evolve(state: PricingStates, event: PricingEvents): PricingStates {
+            switch (event._named) {
+                case "Price of trip was calculated": {
+                    return {
+                        _named: "TripIsPriced",
+                    };
+                }
+                default: {
+                    return state;
+                }
+            }
+        },
+        identify(command: PricingCommands): TripId {
+            throw new Error('TODO: Implement me');
+        },
+        initialState(): PricingStates {
+            return {
+                _named: "TripIsNotPriced",
+            };
+        }
+    };
+};
