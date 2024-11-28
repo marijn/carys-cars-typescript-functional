1b25d5629d9793ef5c835bca6b4cb2ff4f2505f6
Marijn Huizendveld <marijn@huizendveld.com>
2024-11-28 14:36:36 +0100

    :gem: Rename builder.

--- src/Domain/Pricing/TestingMessages.ts
+++ src/Domain/Pricing/TestingMessages.ts
@@ -3,7 +3,7 @@ import {PriceOfTripWasCalculated} from "./PriceOfTripWasCalculated";
 import {durationOfTripFromString} from "./DurationOfTrip";
 import Dinero from "dinero.js";
 
-export const buildPriceOfTripWasCalculated = builderFor<PriceOfTripWasCalculated, "Price of trip was calculated">({
+export const aPriceOfTripWasCalculated = builderFor<PriceOfTripWasCalculated, "Price of trip was calculated">({
     _named: "Price of trip was calculated",
     agreementId: 'agreement:71d3db08-4207-4cd7-9588-7d60f26645bc',
     customerId: 'customer:dae3ca24-b1e6-4f0e-85cb-0c4b9f5fab8b',
