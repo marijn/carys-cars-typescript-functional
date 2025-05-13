005575bfd4264921f7d1b41ee0f33893883c2893
Marijn Huizendveld <marijn@huizendveld.com>
2025-05-13 13:21:00 +0200

    :mag: `Please reserve vehicle`
     - `Vehicle is available`
     - `Vehicle is reserved`

---
+++ src/Domain/Reserving/PleaseReserveVehicle.spec.ts
@@ -0,0 +1,222 @@
+import {describe, test} from "@jest/globals";
+import {CommandHandlingScenario} from "../../Infrastructure/Decider/Testing/CommandHandlingScenario";
+import {runAssertionOnDecider} from "../../Infrastructure/Decider/Testing/runAssertionOnDecider";
+import {LicensePlate} from "../LicensePlate";
+import {LocalDateTime, ZonedDateTime, ZoneId} from "js-joda";
+import {CustomerId} from "../CustomerId";
+import {builderFor} from "../../Infrastructure/Messages/Builder";
+import {Decider} from "../../Infrastructure/Decider/Decider";
+
+type VehicleClass =
+    | 'in and around the city'
+    | 'fun vehicles'
+    | 'long distance trips'
+    | 'moving bulky things'
+
+type VehicleEnteredOperation = {
+    _named: 'Vehicle entered operation',
+
+    /**
+     * @example "DE:M-CC-0001"
+     */
+    vehicle: LicensePlate,
+
+    /**
+     * @example "fun vehicles"
+     */
+    vehicleClass: VehicleClass,
+
+    /**
+     * @example 2024-11-02 16:59:01 Europe/Amsterdam
+     */
+    when: ZonedDateTime
+};
+
+export const aVehicleEnteredOperation = builderFor<VehicleEnteredOperation, "Vehicle entered operation">({
+    _named: 'Vehicle entered operation',
+    vehicle: "DE:M-CC-0001",
+    vehicleClass: "fun vehicles",
+    when: ZonedDateTime.of(
+        LocalDateTime.parse("2024-11-02T16:59:01"),
+        ZoneId.of("UTC+2")
+    ),
+});
+
+type VehicleWasReserved = {
+    _named: "Vehicle was reserved",
+
+    /**
+     * @example "DE:M-CC-0001"
+     */
+    vehicle: LicensePlate,
+
+    /**
+     * @example "fun vehicles"
+     */
+    vehicleClass: VehicleClass,
+
+    /**
+     * @example "customer:dae3ca24-b1e6-4f0e-85cb-0c4b9f5fab8b"
+     */
+    reservedBy: CustomerId,
+
+    /**
+     * @example 2024-11-02 20:19:52.017351 Europe/Amsterdam
+     */
+    when: ZonedDateTime
+};
+
+export const aVehicleWasReserved = builderFor<VehicleWasReserved, "Vehicle was reserved">({
+    _named: "Vehicle was reserved",
+    vehicle: "DE:M-CC-0001",
+    vehicleClass: "fun vehicles",
+    reservedBy: "customer:dae3ca24-b1e6-4f0e-85cb-0c4b9f5fab8b",
+    when: ZonedDateTime.of(
+        LocalDateTime.parse("2024-11-02T20:19:52"),
+        ZoneId.of("UTC+2")
+    )
+});
+
+type ReservationRejectionReason = | "already reserved"
+type VehicleCouldNotBeReserved = {
+    _named: "Vehicle could not be reserved",
+
+    /**
+     * @example "DE:M-CC-0001"
+     */
+    vehicle: LicensePlate,
+
+    /**
+     * @example "fun vehicles"
+     */
+    vehicleClass: VehicleClass,
+
+    /**
+     * @example "customer:dae3ca24-b1e6-4f0e-85cb-0c4b9f5fab8b"
+     */
+    interestedCustomer: CustomerId,
+
+    /**
+     * @example 2024-11-02 20:19:52.017351 Europe/Amsterdam
+     */
+    when: ZonedDateTime,
+
+    /**
+     * @example "already reserved"
+     */
+    reason: ReservationRejectionReason
+};
+
+type PleaseReserveVehicle = {
+    _named: "Please reserve vehicle!",
+
+    /**
+     * @example DE:M-CC-0001
+     */
+    vehicle: LicensePlate,
+
+    /**
+     * @example customer:dae3ca24-b1e6-4f0e-85cb-0c4b9f5fab8b
+     */
+    reservedBy: CustomerId,
+
+    /**
+     * @example 2024-11-02 20:19:52.017351 Europe/Amsterdam
+     */
+    when: ZonedDateTime
+};
+
+type AnyReservingEvent =
+    | VehicleEnteredOperation
+    | VehicleCouldNotBeReserved
+    | VehicleWasReserved
+
+type AnyReservingCommand =
+    | PleaseReserveVehicle
+
+type AnyReservingState = {}
+
+const decider: Decider<AnyReservingCommand, AnyReservingState, AnyReservingEvent, LicensePlate> = {
+    async decide(command: AnyReservingCommand, state: AnyReservingState): Promise<AnyReservingEvent[]> {
+        throw new Error('TODO: Implement me');
+    },
+    evolve(state: AnyReservingState, event: AnyReservingEvent): AnyReservingState {
+        throw new Error('TODO: Implement me');
+    },
+    identify(command: AnyReservingCommand): LicensePlate {
+        throw new Error('TODO: Implement me');
+    },
+    initialState(): AnyReservingState {
+        throw new Error('TODO: Implement me');
+    }
+};
+
+describe('Please reserve vehicle', () => {
+    const scenario = new CommandHandlingScenario<AnyReservingEvent, AnyReservingCommand>()
+
+    test('Vehicle is available', async () => {
+        return scenario
+            .given(
+                aVehicleEnteredOperation()
+                    .with('vehicle', 'NL:GHC-12-A')
+                    .andWith('vehicleClass', 'long distance trips')
+                    .toObject()
+            )
+            .when({
+                _named: "Please reserve vehicle!",
+                vehicle: 'NL:GHC-12-A',
+                reservedBy: "customer:11111111-1111-1111-1111-111111111111",
+                when: ZonedDateTime.of(
+                    LocalDateTime.parse("2024-11-02T20:19:52"),
+                    ZoneId.of("UTC+2")
+                )
+            })
+            .then({
+                _named: "Vehicle was reserved",
+                vehicle: 'NL:GHC-12-A',
+                vehicleClass: 'long distance trips',
+                reservedBy: "customer:11111111-1111-1111-1111-111111111111",
+                when: ZonedDateTime.of(
+                    LocalDateTime.parse("2024-11-02T20:19:52"),
+                    ZoneId.of("UTC+2")
+                )
+            })
+            .assertScenario(runAssertionOnDecider(decider));
+    });
+
+    test('Vehicle is reserved', async () => {
+        return scenario
+            .given(
+                aVehicleEnteredOperation()
+                    .with('vehicle', 'NL:GHC-12-A')
+                    .andWith('vehicleClass', 'long distance trips')
+                    .toObject(),
+                aVehicleWasReserved()
+                    .with('vehicle', 'NL:GHC-12-A')
+                    .andWith('vehicleClass', 'long distance trips')
+                    .andWith('reservedBy', 'customer:11111111-1111-1111-1111-111111111111')
+                    .toObject(),
+            )
+            .when({
+                _named: "Please reserve vehicle!",
+                vehicle: 'NL:GHC-12-A',
+                reservedBy: "customer:22222222-2222-2222-2222-222222222222",
+                when: ZonedDateTime.of(
+                    LocalDateTime.parse("2024-11-02T20:19:53"),
+                    ZoneId.of("UTC+2")
+                )
+            })
+            .then({
+                _named: "Vehicle could not be reserved",
+                vehicle: 'NL:GHC-12-A',
+                vehicleClass: "long distance trips",
+                interestedCustomer: "customer:22222222-2222-2222-2222-222222222222",
+                when: ZonedDateTime.of(
+                    LocalDateTime.parse("2024-11-02T20:19:53"),
+                    ZoneId.of("UTC+2")
+                ),
+                reason: "already reserved"
+            })
+            .assertScenario(runAssertionOnDecider(decider));
+    });
+});
